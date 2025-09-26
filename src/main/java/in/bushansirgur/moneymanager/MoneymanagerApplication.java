02a06a0d880b9ec45e8d06a4355a75389ad250fc
K-TARUN-SAI <ketarajutarunsai2006@gmail.com>
2025-09-26 21:21:32 +0530

    v3

--- src/main/java/in/bushansirgur/moneymanager/MoneymanagerApplication.java
+++ src/main/java/in/bushansirgur/moneymanager/MoneymanagerApplication.java
@@ -2,14 +2,20 @@ package in.bushansirgur.moneymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class MoneymanagerApplication {
+public class MoneymanagerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoneymanagerApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MoneymanagerApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(MoneymanagerApplication.class, args);
+    }
 }
